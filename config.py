4879c677e9b4872f013605f0b6322d84e685a5f5
hyndrong <hyndrong@gmail.com>
2025-05-30 02:26:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()  
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+if not TELEGRAM_TOKEN:
+    raise ValueError("TELEGRAM_TOKEN tidak ditemukan di environment variables")
+ 
 LIBRE_TRANSLATE_URL = os.getenv("LIBRE_TRANSLATE_URL")  
 
 BOT_DESCRIPTION = """  
